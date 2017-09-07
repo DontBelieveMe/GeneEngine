a202ff7de4f75f21f3f84681849ad060ae2f4ea5
DontBelieveMe <barneywilks1@gmail.com>
2017-09-07 19:35:49 +0100

    Bit of cleaning up of X11 windowing code

--- Gene/Engine/Private/Platform/Linux/X11Window.cc
+++ Gene/Engine/Private/Platform/Linux/X11Window.cc
@@ -34,32 +34,68 @@ void X11Window::Create()
 {
     Display *dpy = XOpenDisplay(NULL);
     s_XRoot = DefaultRootWindow(dpy);
-    GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+
+    GLint att[] =
+    {
+        GLX_RGBA,
+        GLX_DEPTH_SIZE,
+        24,
+        GLX_DOUBLEBUFFER,
+        None
+    };
+
     XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
     Colormap cmap = XCreateColormap(dpy, s_XRoot, vi->visual, AllocNone);
+
     XSetWindowAttributes swa;
-    swa.colormap = cmap;
-    swa.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask | StructureNotifyMask;
-    s_XEvtDestroyWIndowMessage =XInternAtom(dpy, "WM_DELETE_WINDOW", False);
-    s_XWindow = XCreateWindow(dpy, s_XRoot, 0, 0, Width(), Height(), 0, vi->depth, InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+    swa.colormap   = cmap;
+    swa.event_mask = ExposureMask |
+                     KeyPressMask |
+                     KeyReleaseMask |
+                     StructureNotifyMask;
+
+
+    s_XWindow = XCreateWindow(dpy,
+                              s_XRoot,
+                              0,
+                              0,
+                              Width(),
+                              Height(),
+                              0,
+                              vi->depth,
+                              InputOutput,
+                              vi->visual,
+                              CWColormap | CWEventMask,
+                              &swa
+    );
+
     XStoreName(dpy, s_XWindow, m_WindowConfig.Title);    
-    m_Display = dpy;
+
+    m_Display    = dpy;
     m_VisualInfo = vi;
+
+    // Configure/register window destroy message
+    s_XEvtDestroyWIndowMessage = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
     XSetWMProtocols(dpy, s_XWindow, &s_XEvtDestroyWIndowMessage, 1);
+
+    // Configure input handlers.
     Input::Mouse::SetPrimaryWindow(this);
     Input::Keyboard::SetPrimaryWindow(this);
+
     m_Window = &s_XWindow;
 }
 
 static void *GetProcAddress(const char *address)
 {
-    return (void*)glXGetProcAddress((GLubyte*)address);
+    return (void*) glXGetProcAddress((GLubyte*)address);
 }
 
 void X11Window::CreateGLContext()
 {
-    GLXContext glc = glXCreateContext((Display*)m_Display, (XVisualInfo*)m_VisualInfo, NULL, GL_TRUE);
-    glXMakeCurrent((Display*)m_Display, s_XWindow, glc);
+    Display *dpy = static_cast<Display*>(m_Display);
+
+    GLXContext glc = glXCreateContext(dpy, (XVisualInfo*)m_VisualInfo, NULL, GL_TRUE);
+    glXMakeCurrent(dpy, s_XWindow, glc);
     m_Context->Context = glc;
 
     GLFLUtil::GLFLInit(GetProcAddress);
@@ -76,57 +112,58 @@ void X11Window::Show()
 void X11Window::PollEvents()
 {
     Display *dpy = static_cast<Display*>(m_Display);
+
     int pending = XPending(dpy);
     while(pending--)
     {
         XEvent evt;
         XNextEvent(dpy, &evt);
+
         switch(evt.type)
         {
-        case ClientMessage:
-        {
-            if(evt.xclient.data.l[0] == s_XEvtDestroyWIndowMessage)
+            case ClientMessage:
             {
-                XDestroyWindow(dpy, evt.xclient.window);
-                m_Running = false;
-                return;
-            }
-            break;
-        }
-        case KeyPress:
-        {
-            KeySym pressSym = XkbKeycodeToKeysym(dpy, evt.xkey.keycode, 0, 0);
-            m_KeyState.KeyMap[pressSym] = true;
-            break;
-        }
-        case KeyRelease:
-        {
-            KeySym releaseSym = XkbKeycodeToKeysym(dpy, evt.xkey.keycode, 0, 0);
-            m_KeyState.KeyMap[releaseSym] = false;
-
-            break;
-        }
-        case ConfigureNotify:
-        {
-            XConfigureEvent confEvent = evt.xconfigure;
-            if(confEvent.width != Width() || confEvent.height != Height())
-            {
-                if(m_Callbacks.Resize)
+                if(evt.xclient.data.l[0] == s_XEvtDestroyWIndowMessage)
                 {
-                    m_WindowConfig.Width = confEvent.width;
-                    m_WindowConfig.Height = confEvent.height;
-                    m_Callbacks.Resize(confEvent.width, confEvent.height);
+                    XDestroyWindow(dpy, evt.xclient.window);
+                    m_Running = false;
+                    return;
                 }
+                break;
             }
-            break;
+            case KeyPress:
+            {
+                KeySym pressSym = XkbKeycodeToKeysym(dpy, evt.xkey.keycode, 0, 0);
+                m_KeyState.KeyMap[pressSym] = true;
+                break;
+            }
+            case KeyRelease:
+            {
+                KeySym releaseSym = XkbKeycodeToKeysym(dpy, evt.xkey.keycode, 0, 0);
+                m_KeyState.KeyMap[releaseSym] = false;
+                break;
+            }
+            case ConfigureNotify:
+            {
+                XConfigureEvent confEvent = evt.xconfigure;
+                if(confEvent.width != Width() || confEvent.height != Height())
+                {
+                    if(m_Callbacks.Resize)
+                    {
+                        m_WindowConfig.Width = confEvent.width;
+                        m_WindowConfig.Height = confEvent.height;
+                        m_Callbacks.Resize(confEvent.width, confEvent.height);
+                    }
+                }
+                break;
+            }
         }
-    }
     }
     
-    int x,y;
-    ::Window a, b;
-    int c, d;
+    int x,y, c, d;
     unsigned e;
+    ::Window a, b;
+
     XQueryPointer(dpy, s_XWindow, &a, &b, &c, &d, &x, &y, &e);
     m_MouseState.Position.X = x;
     m_MouseState.Position.Y = y;
