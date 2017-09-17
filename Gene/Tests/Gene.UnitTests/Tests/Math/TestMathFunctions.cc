745160cd557894428fc540d368a1d030c9be3b13
Barney Wilks <barneywilks1@gmail.com>
2017-09-17 20:52:55 +0100

    Added unit tests for math functions.

---
+++ Gene/Tests/Gene.UnitTests/Tests/Math/TestMathFunctions.cc
@@ -0,0 +1,58 @@
+#include <Catch/catch.hpp>
+
+#include <Math/Functions.h>
+
+using namespace Gene::Math;
+
+TEST_CASE("Test that noone has fiddled with PI (if you have, shame on you)", "[Functions]")
+{
+	REQUIRE(Maths::PI == Approx(3.14159265359f));
+}
+
+TEST_CASE("Test Clamp() clamping to minimum works.", "[MathFunctions]")
+{
+	int min = 12;
+	int value = -123;
+	int max = 56;
+	int output = Maths::Clamp(value, min, max);
+
+	REQUIRE(output == min);
+}
+
+TEST_CASE("Test Clamp() clamping to maximum works.", "[MathFunctions]")
+{
+	int min = 12;
+	int value = 345;
+	int max = 56;
+	int output = Maths::Clamp(value, min, max);
+
+	REQUIRE(output == max);
+}
+
+TEST_CASE("Test Clamp() works when no clamping required.", "[MathFunctions]")
+{
+	int min = 12;
+	int value = 43;
+	int max = 56;
+	int output = Maths::Clamp(value, min, max);
+
+	REQUIRE(output == value);
+}
+
+TEST_CASE("Test ToRadians converts degrees value correctly.", "[MathFunctions]")
+{
+	float degrees = 45;
+
+	float expectedRadians = degrees * (Maths::PI / 180);
+
+	REQUIRE(Maths::ToRadians(degrees) == Approx(expectedRadians));
+}
+
+TEST_CASE("Test ToDegrees converts radian value correctly.", "[MathFunctions]")
+{
+	float radians = 0.345f;
+
+	float expectedDegrees = radians * (180 / Maths::PI);
+
+	REQUIRE(Maths::ToDegrees(radians) == expectedDegrees);
+}
