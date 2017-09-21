0ceee949955622f9c0b3ef092b4f74384ccdf6a5
Barney Wilks <barneywilks1@gmail.com>
2017-09-21 22:00:54 +0100

    Started integrating FreeType again. Working on building library on windows.

--- Gene.Sandbox/Source/Main.cc
+++ Gene.Sandbox/Source/Main.cc
@@ -11,9 +11,14 @@
 #include <GeneCommon.h>
 #include <Graphics/Light.h>
 #include <Input/Keyboard.h>
+#include <ft2build.h>
+
+#include <freetype/freetype.h>
 
 static void CreateTriangle(Gene::Graphics::VertexArray& vao, Gene::Graphics::Buffer** ebo, Gene::Content::GeneModel *model)
 {
+	FT_Library *lib = NULL;
+	FT_Init_FreeType(lib);
 	using namespace Gene::Graphics;
 	using namespace Gene::OpenGL;
 	
