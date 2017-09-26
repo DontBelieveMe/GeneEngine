5ec878477beb929036a2fa441623b742dbb7766a
Barney Wilks <barneywilks1@gmail.com>
2017-09-26 17:14:42 +0100

    -Included Renderer2D in VS .sln
    - Refactored Window.cc -> choosing which windowing backend to use

--- Gene/Engine/Private/Platform/Window.cc
+++ Gene/Engine/Private/Platform/Window.cc
@@ -3,6 +3,16 @@
 #include <Platform/OpenGL.h>
 #include <Graphics/Color.h>
 
+#if defined(GENE_OS_WINDOWS)
+	#include "Win32/Win32Window.h"
+	using PlatformWindow = Gene::Platform::Win32::Win32Window;
+#elif defined(GENE_OS_LINUX)
+    #include "Linux/X11Window.h"
+	using PlatformWindow = Gene::Platform::X11::X11Window;
+#else
+	#error [[GENE Engine Compile Error]]: Unknown Platform/OS! - (Cannot Create Window)
+#endif
+
 using namespace Gene::Platform;
 using namespace Gene::Graphics;
 
@@ -18,12 +28,7 @@ void Window::SetClearColor(Color color)
 		color.LinearR(), color.LinearG(), color.LinearB(), color.LinearA());
 }
 
-#if defined(GENE_OS_WINDOWS)
-	#include "Win32/Win32Window.h"
-	Window *Window::CreateWindow(WindowInfo info) { return new Gene::Platform::Win32::Win32Window(info);  }
-#elif defined(GENE_OS_LINUX)
-    #include "Linux/X11Window.h"
-    Window *Window::CreateWindow(WindowInfo info) { return new Gene::Platform::X11::X11Window(info); }
-#else
-	#error [[GENE Engine Compile Error]]: Unknown Platform/OS! - (Cannot Create Window)
-#endif
+Window *Window::CreateWindow(WindowInfo info) 
+{
+	return new PlatformWindow(info);
+}
