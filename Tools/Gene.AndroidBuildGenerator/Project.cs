2818cbeef0ec1ff6e92ff121ebd0e7cc7ce14547
Barney Wilks <barneywilks1@gmail.com>
2018-01-24 21:08:16 +0000

    Started working on android makefile generator (Codename Agnes XD)

---
+++ Tools/Gene.AndroidBuildGenerator/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gene.AndroidBuildGenerator
+{
+    class Project
+    {
+        public string Name;
+        public List<string> SourceFiles;
+        public List<string> Libraries;
+
+        public Project()
+        {
+            Name = string.Empty;
+            SourceFiles = new List<string>();
+            Libraries = new List<string>();
+        }
+    }
+}
---
+++ Tools/Gene.AndroidBuildGenerator/ProjectReader.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gene.AndroidBuildGenerator
+{
+    class NameConstants
+    {
+        public const string ProjectName = "projectName";
+        public const string SourceFiles = "sourceFiles";
+        public const string Libs = "libs";
+
+    }
+
+    class ProjectReader
+    {
+        public Project ReadProject(JsonReader reader)
+        {
+            Project project = new Project();
+
+            while(reader.Read())
+            {
+                IfPropertyNameIs(reader, NameConstants.ProjectName, () => {
+                    string projectName = reader.ReadAsString();
+                    project.Name = projectName;
+                });
+
+                IfPropertyNameIs(reader, NameConstants.SourceFiles, () => {
+                    project.SourceFiles = ReadStringArray(reader);
+                });
+
+                IfPropertyNameIs(reader, NameConstants.Libs, () => {
+                    project.Libraries = ReadStringArray(reader);
+                });
+            }
+
+            return project;
+        }
+
+        private List<string> ReadStringArray(JsonReader reader)
+        {
+            List<string> strings = new List<string>();
+            while(reader.TokenType != JsonToken.EndArray)
+            {
+                reader.Read();
+                if (reader.TokenType == JsonToken.String)
+                {
+                    strings.Add(reader.Value.ToString());
+                }
+            }
+            return strings;
+        }
+
+        private void IfPropertyNameIs(JsonReader reader, string name, Action doThis)
+        {
+            if (reader.TokenType != JsonToken.PropertyName)
+                return;
+
+            if(reader.Value.ToString() == name)
+            {
+                doThis();
+            }
+        }
+    }
+}
