18f35550b1486508362dec70521e53e4fe4a8151
Barney Wilks <barneywilks1@gmail.com>
2018-03-27 19:53:36 +0100

    [TASK: Android build] -> Improved AndroidBuild.bat script with error checking

--- Setup/Android/AndroidBuild.bat
+++ Setup/Android/AndroidBuild.bat
@@ -1,15 +1,5 @@
 @echo off
 
-if "%1" == "clean" (
-	rmdir bin /s /q
-	rmdir libs /s /q
-	del build.xml
-	del local.properties
-	del proguard-project.txt
-	del project.properties
-	exit /b
-)
-
 :: Set a bunch of variables (obvs)
 set build_dir=..\..\Build
 set src_dir=..\
@@ -17,6 +7,12 @@ set ndk_dir=I:/android-ndk-r12b
 
 set app_name=Gene.Sandbox
 set package_name=com.gene.sandbox
+
+if "%1" == "clean" (
+	rmdir %build_dir% /s /q
+	cd %~dp0
+	exit /b 0
+)
 
 set abi=%1
 
@@ -32,19 +28,22 @@ cd %build_dir%
 :: Generate android project files (needed for ant)
 call android update project --path . --target android-24 -n Gene.Sandbox
 
+call :check_for_errors
+
 :: Generate the makefile
 call cmake -DCMAKE_TOOLCHAIN_FILE=android.toolchain.cmake -DANDROID_ABI=%abi% DCMAKE_BUILD_TOOL="%ndk_dir%/prebuilt/windows-x86_x64/bin/make.exe" -G"MinGW Makefiles" -DANDROID_NDK=%ndk_dir% -DANDROID_NATIVE_API_LEVEL=android-24 -DLIBRARY_OUTPUT_PATH_ROOT=. -B. -H%src_dir%
+
+call :check_for_errors
 
 :: Build the project
 call cmake --build .
 
+call :check_for_errors
+
 :: Run ant (generate APK and package resources etc...)
 call ant debug
 
-if "%2" == "" (
-	cd %~dp0
-	exit /b
-)
+call :check_for_errors
 
 :: If we need to deploy upload the APK to the device and then open it
 if "%2" == "deploy" (
@@ -57,3 +56,10 @@ if "%2" == "deploy" (
 )
 
 cd %~dp0
+
+:check_for_errors
+if %errorlevel% neq 0 (
+	cd %~dp0
+	exit /b %errorlevel%
+)
+exit /b 0
