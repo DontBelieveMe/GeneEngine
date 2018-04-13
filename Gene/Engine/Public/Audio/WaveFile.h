b1e1176227d1f4a01ef052230c64e360feeffe03
Barney Wilks <barneywilks1@gmail.com>
2018-04-13 14:19:51 +0100

    [TASK: Audio] -> Done some formatting of WaveFile.h

--- Gene/Engine/Public/Audio/WaveFile.h
+++ Gene/Engine/Public/Audio/WaveFile.h
@@ -10,29 +10,30 @@
 namespace Gene { namespace Audio {
 	class WaveFile : public IResource {
 	private:
-		int m_AudioFormat;
-		int m_NumChannels;
-		int m_SampleRate;
-		int m_ByteRate;
-		int m_BlockAlign;
-		int m_BitsPerSample;
+		int				m_AudioFormat;
+		int				m_NumChannels;
+		int				m_SampleRate;
+		int				m_ByteRate;
+		int				m_BlockAlign;
+		int				m_BitsPerSample;
 
-		uint8_t *m_AudioData;
-		int m_AudioDataSize;
+		uint8_t		   *m_AudioData;
+		int			    m_AudioDataSize;
 
-		ALuint m_SourceId;
-		ALuint m_BufferId;
+		ALuint			m_SourceId;
+		ALuint  		m_BufferId;
 
-		ALenum GetALFormat();
+		ALenum			GetALFormat();
+
 	public:
-		void Load(const char *filepath);
-		virtual void Destroy();
+		void			Load(const char *filepath);
+		virtual void	Destroy();
 
-		void Loop(bool shouldLoop);
-		void SetGain(float gain);
+		void			Loop(bool shouldLoop);
+		void			SetGain(float gain);
 
-		bool IsPlaying();
+		bool			IsPlaying();
 
-		ALuint GetSourceId() const { return m_SourceId; }
+		ALuint			GetSourceId() const { return m_SourceId; }
 	};
 }}
