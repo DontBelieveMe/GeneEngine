681847564dd2428ceab6b530e89ca98780963b20
Barney Wilks <barneywilks1@gmail.com>
2018-06-09 23:43:20 +0100

    Removed debug (android log) file, adding logging utility batch file and sorted out the commonly used commands list.

--- Setup/Android/Commands.bat
+++ Setup/Android/Commands.bat
@@ -1,3 +1,11 @@
-AndroidBuild.bat arm64-v8a install (will only install on device and not open)
-AndroidBuild.bat arm64-v8a deploy (will install and open)
-adb logcat Gene:V *:S (Filters to only include Gene logs and prints to stdout)
+:: will only install on device and not open
+AndroidBuild.bat arm64-v8a install 
+
+:: will install on device and open the app
+AndroidBuild.bat arm64-v8a deploy 
+
+:: filters to only include Gene logs and prints to stdout
+adb logcat Gene:V *:S 
+
+:: clears the log buffer
+adb logcat -c
