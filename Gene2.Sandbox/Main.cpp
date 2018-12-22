0eb1e9ee5e38efa590bc543732a17420402dcdfe
Barney Wilks <barneywilks1@gmail.com>
2018-12-22 21:49:51 +0000

    Removed unused includes from Main.cpp

--- Gene2.Sandbox/Main.cpp
+++ Gene2.Sandbox/Main.cpp
@@ -1,13 +1,10 @@
-#include <Gene2/Core/StdLib/String.hpp>
 #include <Gene2/Debug/Logging.hpp>
-#include <Gene2/Debug/Assert.hpp>
-
 #include <Gene2/Platform/IWindow.hpp>
 
 int main()
 {
 	G2_CORE_LOGGER()->AddRoute<g2::ConsoleLoggerRoute>();
-
+	
 	g2::IWindow* window = g2::IWindow::Create(g2::WindowConfig(800, 600, "Gene2 Demo!"));
 	window->Show();
 
